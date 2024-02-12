c0ee878aa9ac9916f54bd8be0c72fd9959a935db
ArkoSammy12 <jamesvillarreal12@gmail.com>
2024-02-11 21:31:57 -0500

    The big refactor commit

---
+++ src/main/java/xd/arkosammy/creeperhealing/explosions/ducks/ExplosionAccessor.java
@@ -0,0 +1,12 @@
+package xd.arkosammy.creeperhealing.explosions.ducks;
+
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.world.World;
+
+public interface ExplosionAccessor {
+
+    World creeper_healing$getWorld();
+
+    DamageSource creeper_healing$getDamageSource();
+
+}
